22f94b7f6f0ae5f171535630ac6b0a00c2f2b31e
ShawnGao <2519306859@qq.com>
2025-04-11 14:07:37 +0800

    build: Modify the system's default installation options and optimize the installation process
    - Changed system default installation option from Y to N
    - Fix the default value setting logic of variable REPLY
    - Added handling of different operating systems:
      - macOS does not execute ldconfig
      - Execute ldconfig on other systems
    - Uncomment and modify the CMAKE_C_FLAGS setting in CMakeLists.txt

--- build.sh
+++ build.sh
@@ -19,14 +19,20 @@ else
   echo -e "\033[0;31mBuild Failed!"
   exit 1
 fi
-echo -e "\033[0;38mDo you want to install this lib to system? (Y/N) [default in 5s: Y]: "
+echo -e "\033[0;38mDo you want to install this lib to system? (Y/N) [default in 5s: N]: "
 read -t 5 REPLY
-if [ -z "$age" ]; then
-  REPLY=Y
+if [ -z $REPLY ]; then
+  REPLY="N"
 fi
 if [ $REPLY = Y ] || [ $REPLY = y ]; then
   echo -e "\033[0;33mInstalling..."
   sudo make install
+  system=`uname`
+  if [ "$system" == "Darwin" ]; then
+    # Mac OS
+  else
+    ldconfig
+  fi
   if [ $? -eq 0 ]; then
     echo -e "\033[0;32mInstall Successful!\033[0m"
   else
